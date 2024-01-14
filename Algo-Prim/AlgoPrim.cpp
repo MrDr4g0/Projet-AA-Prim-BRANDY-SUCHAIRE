ec2ac9b4ca224957dd7271600f0702c520f449d6
MrDr4g0 <lucas.brandy@laposte.net>
2024-01-14 16:38:08 +0100

    Ajoutez des fichiers projet.

---
+++ Algo-Prim/AlgoPrim.cpp
@@ -0,0 +1,11 @@
+// Algo-Prim.cpp : Définit les fonctions de la bibliothèque statique.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "AlgoPrim.h"
+
+// TODO: Il s'agit d'un exemple de fonction de bibliothèque
+void fnAlgoPrim()
+{
+}
